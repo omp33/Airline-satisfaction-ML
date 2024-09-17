41452c59746838f8c11d9fbcc9a38a2120b07908
omp33 <omprakashpatel2933@gmail.com>
2024-09-17 11:53:54 -0800

    datasets and some code

--- Project.py
+++ Project.py
@@ -6,3 +6,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
 from tensorflow import keras
+
+# Load the dataset
+data = pd.read_csv('satisfaction train.csv')
+print(data.shape)
