e82c396f3e49ae0e43c8eab22640ce57c8ff7078
omp33 <omprakashpatel2933@gmail.com>
2024-09-17 11:23:07 -0800

    important python libs

--- Project.py
+++ Project.py
@@ -2,3 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import tensorflow as tf 
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import classification_report, confusion_matrix
+from sklearn.model_selection import train_test_split
+from tensorflow import keras
